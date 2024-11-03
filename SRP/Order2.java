754d1675ef8ec3777be10114a2d6eccede88fa38
LAPTOP-409V4KK7\andhika pratama s <andhikaadlie@gmail.com>
2024-11-03 17:42:23 +0700

    tambah file SRP

---
+++ SRP/Order2.java
@@ -0,0 +1,17 @@
+class Order2 { 
+ void calculateTotalSum() { 
+ } 
+ 
+ void getItems() { 
+ } 
+ 
+ void getItemCount() { 
+ } 
+ 
+ void addItem(Item item) { 
+ 
+ } 
+ 
+ void deleteItem(Item item) { 
+ } 
+}
