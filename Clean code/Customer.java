15160eddfff9dcd9e39169aa8ab2f1f349eb477c
LAPTOP-409V4KK7\andhika pratama s <andhikaadlie@gmail.com>
2024-12-06 13:56:01 +0700

    Clean code

---
+++ Clean code/Customer.java
@@ -0,0 +1,8 @@
+package Clean code;
+
+class Customer {
+    private Date generationTimestamp;
+    private Date modificationTimestamp;
+    private final String recordId = "102";
+   /* ... */
+   }
---
+++ Clean code/Point.java
@@ -0,0 +1,6 @@
+package Clean code;
+
+public class Point {
+    public double x;
+    public double y;
+}
